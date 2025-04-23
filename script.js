32825456cda9f4e998076974ca3c6a740e142799
fikri-afk <julianmlbb29@gmail.com>
2025-04-23 17:18:36 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-  const bottles = document.querySelectorAll(".bottle");
-  bottles.forEach((bottle, i) => {
-    setTimeout(() => {
-      bottle.classList.add("show");
-    }, i * 300); // delay biar animasinya masuk satu-satu
-  });
-});
